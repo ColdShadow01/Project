14338791c996c501da07ad3345fa11a930d2972b
ColdShadow01 <185036448+ColdShadow01@users.noreply.github.com>
2026-05-30 17:06:59 +0800

    Clean up comments in InvalidInputException.java
    Removed creator and tester comments from InvalidInputException.

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,5 +1,5 @@
-// Creator: Wan Adam
-// Tester: Nisa
+// Creator: 
+// Tester: 
 // Description: Custom exception to catch invalid user inputs during the quiz
 public class InvalidInputException extends Exception {
   public InvalidInputException(String message) {
