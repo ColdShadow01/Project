7e6e9222bf6d54d09804d18face324802f7a3b6d
ColdShadow01 <185036448+ColdShadow01@users.noreply.github.com>
2026-05-30 17:01:47 +0800

    Rename InvalidScoreException to InvalidInputException

--- InvalidScoreException.java
+++
@@ -1,8 +0,0 @@
-// Creator: Wan Adam
-// Tester: Nisa
-// Description: Custom exception to catch grading logic errors and prevent invalid data saves
-public class InvalidScoreException extends Exception {
-  public InvalidScoreException(String message) {
-    super(message);
-  }
-}
